76f6b7d8ec7cd16d796c4a28850be38461e5f77e
Aarav Porwal <69109608+aaravporwal@users.noreply.github.com>
2024-09-07 12:58:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def calculate_trip_cost(distance, mileage, fuel_price):
+    fuel_consumption = distance / mileage
+    trip_cost = fuel_consumption * fuel_price
+    return trip_cost
+
+distance = float(input("Enter the distance of the trip in kilometers: "))
+print()
+mileage = float(input("Enter the mileage of your car in kilometers per liter: "))
+print()
+fuel_price = float(input("Enter the price of fuel per liter: "))
+print()
+total_cost = calculate_trip_cost(distance, mileage, fuel_price)
+
+print(f"The total cost of the trip will be {total_cost:.2f} rupees")
